6f5b9473ee666eb284e62fe3dea9a0231eee251b
rxan01 <rteodoro742@gmail.com>
2025-03-27 21:03:14 -0300

    commit 27/03

--- JAVASCRIPT/telaHome.js
+++ JAVASCRIPT/telaHome.js
@@ -6,6 +6,10 @@ function redirectToRegister() {
     window.location.href = "/HTML/telaCadastro.html"; 
 }
 
+function redirectToVoltarH() {
+    window.location.href = "/HTML/telaHome.html"; 
+}
+
 function redirectToAdicionar() {
     window.location.href = ""; 
 }
