2103d6f44976e4d1a731b94baf9dbed4717f55f4
Gcz14 <guilhermeribeiroa2004@gmail.com>
2025-03-31 16:15:35 -0300

    tela adm com todas solicitações

--- JAVASCRIPT/direcionamento.js
+++ JAVASCRIPT/direcionamento.js
@@ -7,7 +7,13 @@ function redirectToComprar() {
     window.location.href = "/HTML/telaProdutoUnico.html"; 
 }
 
+
 function redirectToCd() {
     window.location.href = "/HTML/telaCarrinhoEntrega.html"; 
 }
-
+function redirectToEstoque() {
+    window.location.href = "/HTML ADM/telaEstoque.html"; 
+}
+function redirectToAdicionandoProduto() {
+    window.location.href = "/HTML ADM/telaAdicionandoProduto.html"; 
+}
